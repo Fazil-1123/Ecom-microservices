472420f05b5f7aab197b4ac994952b986b57ecfd
Fazil-1123 <152527893+Fazil-1123@users.noreply.github.com>
2025-09-13 13:41:41 +0200

    feat: migrating rabbitmq to kafka

--- notification/src/main/java/com/ecommerce/notification/OrderEventConsumer.java
+++ notification/src/main/java/com/ecommerce/notification/OrderEventConsumer.java
@@ -1,16 +1,30 @@
 package com.ecommerce.notification;
 
 import com.ecommerce.notification.dtos.OrderCreatedEvent;
-import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Service;
+
+import java.util.function.Consumer;
 
 
 @Service
 public class OrderEventConsumer {
 
-    @RabbitListener(queues = "${rabbitmq.queue.name}")
-    public void handleOrderEvent(OrderCreatedEvent orderEvent) {
-        System.out.println(orderEvent);
+    private static final Logger logger = LoggerFactory.getLogger(OrderEventConsumer.class);
 
+//    @RabbitListener(queues = "${rabbitmq.queue.name}")
+//    public void handleOrderEvent(OrderCreatedEvent orderEvent) {
+//        System.out.println(orderEvent);
+//
+//    }
+
+    @Bean
+    public Consumer<OrderCreatedEvent> orderCreatedEventConsumer(){
+        return event -> {
+            logger.info("Received order id : {}",event.getId());
+            logger.info("Received order amount : {}",event.getTotalAmount());
+        };
     }
 }
